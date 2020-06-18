d96cfa98d02e10b6294d425fdf08e07a5a0a7acd
ue-sho <tssu45@gmail.com>
2020-06-18 22:29:04 +0900

    annotate機能を追加

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'pry-byebug'
+  gem 'pry-byebug'    # デバッグするのに便利なやつ
+  gem 'annotate'      # モデルのファイルに、実際のデータベースの構造がコメントとして記載される
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  title      :string(255)
+#  body       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Board < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 10 } # presence : 未入力だめ
-  validates :title, presence: true, length: { maximum: 30 } # validationはカスタムも作ることができる
+  validates :name, presence: true, length: { maximum: 10 }   # presence : 未入力はだめ！の機能
+  validates :title, presence: true, length: { maximum: 30 }  # validationはカスタムも作ることができる
   validates :body, presence: true, length: { maximum: 1000 }
 end
